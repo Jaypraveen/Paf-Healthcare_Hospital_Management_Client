8c72580d0f767743ec01a6ada15e29d3c59f158d
Jaypraveen <praveen.fernando1998@gmail.com>
2020-05-06 17:07:04 +0530

    complete project

--- WebContent/Hospital_Insert.jsp
+++ WebContent/Hospital_Insert.jsp
@@ -5,30 +5,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Hospital Registation</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Hospital Registation</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css"
+	href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css"
+	href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<link rel="stylesheet" type="text/css"
+	href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<link rel="stylesheet" type="text/css"
+	href="vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<link rel="stylesheet" type="text/css"
+	href="vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<link rel="stylesheet" type="text/css"
+	href="vendor/select2/select2.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" type="text/css"
+	href="vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
 <link rel="stylesheet"
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -43,77 +50,77 @@
 
 	<div class="container-contact100">
 		<div class="wrap-contact100">
-			<form class="contact100-form validate-form" id="formHospital" name="formHospital" method="post" action="Hospital_Insert.jsp">
-				<span class="contact100-form-title">
-					Hospital Registation
-				</span>
-
-			<label class="label-input100" for="email">Hospital Name</label>
+			<form class="contact100-form validate-form" id="formHospital"
+				name="formHospital" method="post" action="Hospital_Insert.jsp">
+				<span class="contact100-form-title"> Hospital Registation </span> <label
+					class="label-input100" for="email">Hospital Name</label>
 				<div class="wrap-input100 validate-input">
-					<input  class="input100"id="hosName" name="hosName" type="text" required>
-					<span class="focus-input100"></span>
+					<input class="input100" id="hosName" name="hosName" type="text"
+						required> <span class="focus-input100"></span>
 				</div>
 
 				<label class="label-input100" for="email">Contact No</label>
-				<div class="wrap-input100 validate-input" >
+				<div class="wrap-input100 validate-input">
 					<input class="input100" id="hosTelNo" name="hosTelNo" type="text"
-							placeholder="0xxxxxxxxx" maxlength="10" pattern="^\d{10}$"
-							class="form-control form-control-sm" required>
-					<span class="focus-input100"></span>
+						placeholder="0xxxxxxxxx" maxlength="10" pattern="^\d{10}$"
+						class="form-control form-control-sm" required> <span
+						class="focus-input100"></span>
 				</div>
 
 				<label class="label-input100" for="phone">Address</label>
 				<div class="wrap-input100">
-					<input  class="input100"id="hosAddress" name="hosAddress" type="text"
-							class="form-control form-control-sm" require>
+					<input class="input100" id="hosAddress" name="hosAddress"
+						type="text" class="form-control form-control-sm" require>
 					<span class="focus-input100"></span>
 				</div>
 
 				<label class="label-input100" for="email">E-mail</label>
 				<div class="wrap-input100 validate-input">
-					<input  class="input100" id="hosEmail" name="hosEmail" type="text"
-							class="form-control form-control-sm" required>
-					<span class="focus-input100"></span>
+					<input class="input100" id="hosEmail" name="hosEmail" type="text"
+						class="form-control form-control-sm" required> <span
+						class="focus-input100"></span>
 				</div>
 
 				<div class="container-contact100-form-btn">
 					<input id="btnSave" name="btnSave" type="button" value="Save"
-							class="btn btn-primary"> <input type="hidden"
-							id="hidHospitalIDSave" name="hidHospitalIDSave" value="">
-							
+						class="btn btn-primary"> <input type="hidden"
+						id="hidHospitalIDSave" name="hidHospitalIDSave" value="">
+
 				</div>
-				<div id="alertSuccess" class="alert alert-success" style="margin-left: 144px;"></div>
-	<div id="alertError" class="alert alert-danger" style="margin-left: 144px;"></div>
+				<div id="alertSuccess" class="alert alert-success"
+					style="margin-left: 144px;"></div>
+				<div id="alertError" class="alert alert-danger"
+					style="margin-left: 144px;"></div>
 			</form>
 
-			<div class="contact100-more flex-col-c-m" style="background-image: url('images/img1.jpg');">
-				</div>
-			</div>
-			
-			
-			<div id="divItemsGrid" style="margin-top: 30px;">
-		<%
+			<div class="contact100-more flex-col-c-m"
+				style="background-image: url('images/img1.jpg');"></div>
+		</div>
+
+
+		<div id="divItemsGrid" style="margin-top: 30px;">
+			<%
 			Hospital h1 = new Hospital();
 			out.print(h1.readHospital());
 		%>
-	</div>
-	
 		</div>
-		
+
+	</div>
+
 	</div>
 
 
 
 	<div id="dropDownSelect1"></div>
 
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
 	<script>
 		$(".selection-2").select2({
@@ -121,15 +128,16 @@
 			dropdownParent: $('#dropDownSelect1')
 		});
 	</script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/daterangepicker/moment.min.js"></script>
 	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+	<script async
+		src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
 	<script>
 	  window.dataLayer = window.dataLayer || [];
 	  function gtag(){dataLayer.push(arguments);}
--- src/model/Hospital.java
+++ src/model/Hospital.java
@@ -8,143 +8,145 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Hospital {
-    //A common method to connect to the DB
-    dbconnect obj = new dbconnect();
+	// A common method to connect to the DB
+	dbconnect obj = new dbconnect();
 
-    public String insertHospital(String hosName, String hosTelNo, String hosAddress, String hosEmail) {
+	public String insertHospital(String hosName, String hosTelNo, String hosAddress, String hosEmail) {
 
-        String output = "";
-        try {
-            Connection con = obj.connect();
-            if (con == null) {
-                return "Error while connecting to the database for inserting.";
-            }
-            // create a prepared statement
-            String query = " insert into hospitals(`hosName`,`hosTelNo`,`hosAddress`,`hosEmail`)" +
-                " values (?, ?, ?, ?)";
-            PreparedStatement preparedStmt = con.prepareStatement(query);
+		String output = "";
+		try {
+			Connection con = obj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into hospitals(`hosName`,`hosTelNo`,`hosAddress`,`hosEmail`)"
+					+ " values (?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
 
-            preparedStmt.setString(1, hosName);
-            preparedStmt.setString(2, hosTelNo);
-            preparedStmt.setString(3, hosAddress);
-            preparedStmt.setString(4, hosEmail);
-            preparedStmt.execute();
-            con.close();
-            
-            String newHospital = readHospital(); 
-            output = "{\"status\":\"success\",\"data\": \"" +newHospital + "\"}";
-            
-            System.out.println("Inserted successfully.......................................");
-        } catch (Exception e) {
-            output = "{\"status\":\"error\", \"data\":\"Error while inserting the Hospitals.\"}";
-                System.err.println(e.getMessage());
-        }
-        return output;
-    }
-    
-    public String readHospital() {
-        String output = "";
-        try {
-            Connection con = obj.connect();
-            if (con == null) {
-                return "Error while connecting to the database for reading.";
-            }
-            // Prepare the html table to be displayed
-            output ="<table border='1'><tr><th>Hospital Name</th><th>Tel No</th><th>"
+			preparedStmt.setString(1, hosName);
+			preparedStmt.setString(2, hosTelNo);
+			preparedStmt.setString(3, hosAddress);
+			preparedStmt.setString(4, hosEmail);
+			preparedStmt.execute();
+			con.close();
+
+			String newHospital = readHospital();
+			output = "{\"status\":\"success\",\"data\": \"" + newHospital + "\"}";
+
+			System.out.println("Inserted successfully.......................................");
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\":\"Error while inserting the Hospitals.\"}";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String readHospital() {
+		String output = "";
+		try {
+			Connection con = obj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Hospital Name</th><th>Tel No</th><th>"
 					+ "Address</th><th>Email</th><th>Update</th><th>Remove</th></tr>";
-            String query = "select * from hospitals";
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            // iterate through the rows in the result set
-            while (rs.next()) {
-                String hosID = Integer.toString(rs.getInt("hosID"));
-                String hosName = rs.getString("hosName");
-                String hosTelNo = rs.getString("hosTelNo");
-                String hosAddress = rs.getString("hosAddress");
-                String hosEmail = rs.getString("hosEmail");
-                hosName = hosName.replace('+', ' ');
-     			hosEmail=hosEmail.replaceAll("%40","@");
-			    hosAddress=hosAddress.replaceAll("%2C",",");
- 				hosAddress=hosAddress.replace('+','/');
+			String query = "select * from hospitals";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String hosID = Integer.toString(rs.getInt("hosID"));
+				String hosName = rs.getString("hosName");
+				String hosTelNo = rs.getString("hosTelNo");
+				String hosAddress = rs.getString("hosAddress");
+				String hosEmail = rs.getString("hosEmail");
+				hosName = hosName.replace('+', ' ');
+				hosEmail = hosEmail.replaceAll("%40", "@");
+				hosAddress = hosAddress.replaceAll("%2C", ",");
+				hosAddress = hosAddress.replace('+', '/');
 
-                // Add into the html table
-                output += "<tr><td><input id='hidHospitalIDUpdate'name='hidHospitalIDUpdate'type='hidden' value='" + hosID + "'>" +
-                    hosName + "</td>";
-                output += "<td>" + hosTelNo + "</td>";
-                output += "<td>" + hosAddress + "</td>";
-                output += "<td>" + hosEmail + "</td>";
+				// Add into the html table
+				output += "<tr><td><input id='hidHospitalIDUpdate'name='hidHospitalIDUpdate'type='hidden' value='"
+						+ hosID + "'>" + hosName + "</td>";
+				output += "<td>" + hosTelNo + "</td>";
+				output += "<td>" + hosAddress + "</td>";
+				output += "<td>" + hosEmail + "</td>";
 
-                // buttons
-                output += "<td><input name='btnUpdate'type='button' "
+				// buttons
+				output += "<td><input name='btnUpdate'type='button' "
 						+ "value='Update'class='btnUpdate btn btn-warning'></td>"
 						+ "<td><input name='btnRemove'type='button' "
-						+ "value='Remove'class='btnRemove btn btn-danger'data-hospitalid='"+ hosID + "'>" + "</td></tr>";
-                
-            }
-            con.close();
-            // Complete the html table
-            output += "</table>";
-        } catch (Exception e) {
-            output = "Error while reading the Hospitals.";
-            System.err.println(e.getMessage());
-        }
-        return output;
-    }
+						+ "value='Remove'class='btnRemove btn btn-danger'data-hospitalid='" + hosID + "'>"
+						+ "</td></tr>";
 
-    public String updateHospital(String ID, String hName, String contactNo, String address, String email) {
-        System.out.println("Update method...............................................................................");
-        String output = "";
-        try {
-            Connection con = obj.connect();
-            if (con == null) {
-                return "Error while connecting to the database for updating.";
-            }
-            //update
-            // create a prepared statement
-            String query = "UPDATE hospitals SET hosName=?,hosTelNo=?,hosAddress=?,hosEmail=? WHERE hosID=?";
-            PreparedStatement preparedStmt = con.prepareStatement(query);
-            // binding values
-            preparedStmt.setString(1, hName);
-            preparedStmt.setString(2, contactNo);
-            preparedStmt.setString(3, address);
-            preparedStmt.setString(4, email);
-            preparedStmt.setInt(5, Integer.parseInt(ID));
-            // execute the statement
-            preparedStmt.execute();
-            con.close();
-            
-            String newHospital = readHospital(); 
-            
-            output = "{\"status\":\"success\", \"data\": \"" +newHospital + "\"}";
-        } catch (Exception e) {
-            output = "{\"status\":\"error\", \"data\":\"Error while updating the Hospitals.\"}";
-            System.err.println(e.getMessage());
-        }
-        return output;
-    }
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the Hospitals.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 
-    public String deleteHospital(String hosID) {
-        String output = "";
-        try {
-            Connection con = obj.connect();
-            if (con == null) {
-                return "Error while connecting to the database for deleting.";
-            }
-            // create a prepared statement
-            String query = "delete from hospitals where hosID=?";
-            PreparedStatement preparedStmt = con.prepareStatement(query);
-            // binding values
-            preparedStmt.setInt(1, Integer.parseInt(hosID));
-            // execute the statement
-            preparedStmt.execute();
-            con.close();
-            String newHospital = readHospital();
-            output = "{\"status\":\"success\", \"data\": \"" +newHospital + "\"}";
-        } catch (Exception e) {
-            output = "{\"status\":\"error\", \"data\":\"Error while deleting the Hospital.\"}"; 
-            System.err.println(e.getMessage());
-        }
-        return output;
-    }
+	public String updateHospital(String ID, String hName, String contactNo, String address, String email) {
+		System.out.println(
+				"Update method...............................................................................");
+		String output = "";
+		try {
+			Connection con = obj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+			// update
+			// create a prepared statement
+			String query = "UPDATE hospitals SET hosName=?,hosTelNo=?,hosAddress=?,hosEmail=? WHERE hosID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setString(1, hName);
+			preparedStmt.setString(2, contactNo);
+			preparedStmt.setString(3, address);
+			preparedStmt.setString(4, email);
+			preparedStmt.setInt(5, Integer.parseInt(ID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			String newHospital = readHospital();
+
+			output = "{\"status\":\"success\", \"data\": \"" + newHospital + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\":\"Error while updating the Hospitals.\"}";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String deleteHospital(String hosID) {
+		String output = "";
+		try {
+			Connection con = obj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+			// create a prepared statement
+			String query = "delete from hospitals where hosID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(hosID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			String newHospital = readHospital();
+			output = "{\"status\":\"success\", \"data\": \"" + newHospital + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\":\"Error while deleting the Hospital.\"}";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 
 }
